b517872ee1eb42f1933af1d06cdd8f6db92e5749
zhangjd_home <527732938@qq.com>
2022-03-31 02:01:33 +0830

    分干添加

--- src/main/java/github/javaguide/springbootfilter/test/Test2.java
+++ src/main/java/github/javaguide/springbootfilter/test/Test2.java
@@ -3,5 +3,7 @@ package github.javaguide.springbootfilter.test;
 public class Test2 {
     public static  void  main(String[] args){
         System.out.println("create branch");
+        System.out.println("create 分支");
+        System.out.println("分支添加");
     }
 }
